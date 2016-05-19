19d9729e8296c44f2a5af8d10c2021112d0e090b
Andreas Reuter <andreas.reuter@fu-berlin.de>
2016-05-19 22:02:12 +0200

    Replace python generated table with DataTables (js lib). Table data is now in visibility.json

---
+++ visi_table.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+    $.ajax({
+            url: "visibility.json",
+            success: create_table
+    });
+});
+
+
+function create_table(rows) {
+
+    columns = []
+    header_row = rows.shift()
+    header_row.forEach(function(entry){
+        columns.push({title: entry});
+    });
+
+    $("#visi_table").DataTable( {
+        data: rows,
+        columns: columns,
+        order: [[ 0, "asc" ]],
+        iDisplayLength: 50,
+        createdRow: color_row
+    });
+}
+
+function color_row(row, data, index) {
+    for(i = 2;i<data.length;i++){
+        if(data[i] == "False"){
+            $('td', row).eq(i).addClass('not_visible');
+        } else {
+            $('td', row).eq(i).addClass('visible');
+        }
+    }
+}
